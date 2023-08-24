f7c6f391ca6581b16f2e96d4bbb7e735afcddf21
mark.abrams <mark.abrams@hitachivantara.com>
2023-08-24 12:53:10 +0100

    Switch the Functions endpoint on port 7071 back to "localhost".

--- LogicAppUnit/Hosting/TestEnvironment.cs
+++ LogicAppUnit/Hosting/TestEnvironment.cs
@@ -41,7 +41,7 @@ namespace LogicAppUnit.Hosting
         /// <summary>
         /// The test host URI.
         /// </summary>
-        public static readonly string FlowV2TestHostUri = (new UriBuilder(Uri.UriSchemeHttp, TestEnvironment.MachineHostName, 7071).Uri.ToString()).TrimEnd('/');
+        public static readonly string FlowV2TestHostUri = (new UriBuilder(Uri.UriSchemeHttp, "localhost", 7071).Uri.ToString()).TrimEnd('/');
 
         /// <summary>
         /// The mock test host URI.
@@ -51,7 +51,7 @@ namespace LogicAppUnit.Hosting
         /// <summary>
         /// The test host URI.
         /// </summary>
-        public static readonly string FlowV2TestManagementHostUri = (new UriBuilder(Uri.UriSchemeHttp, TestEnvironment.MachineHostName, 7071).Uri.ToString()).TrimEnd('/');
+        public static readonly string FlowV2TestManagementHostUri = (new UriBuilder(Uri.UriSchemeHttp, "localhost", 7071).Uri.ToString()).TrimEnd('/');
 
         /// <summary>
         /// Flow runtime webhook extension URI management base path.
